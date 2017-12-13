4bfc02e92d21ebccd3bac38e427acb170a1bda8d
Anton Loukianov <aloukian@stanford.edu>
2017-12-13 14:23:15 -0800

    add pkg_chk for updates and change layout of directory

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -18,3 +18,5 @@ fi
 export PS1="\n\[\033[38;5;1m\]\u\[$(tput sgr0)\]@\[\033[38;5;5m\]\h\[$(tput sgr0)\] \[\033[38;5;4m\]\w\[$(tput sgr0)\]\n> "
 export PATH=$HOME/.local/bin:$HOME/.local/sbin:$PATH
 export MANPATH=$HOME/.local/man:$MANPATH
+export PKGSRCDIR=$HOME/pkgsrc
+export PKGCHK_CONF=$HOME/.local/etc/pkgchk.conf
--- bootstrap-pkgsrc.sh
+++ bootstrap-pkgsrc.sh
@@ -3,17 +3,18 @@
 # pull the ports tree from github
 # this takes quite a while
 PKGSRC_URI=https://github.com/NetBSD/pkgsrc.git
-git clone $PKGSRC_URI $HOME/.local/pkgsrc
+git clone $PKGSRC_URI $HOME/pkgsrc
 
 # bootstrap the ports
-cd $HOME/.local/pkgsrc/bootstrap
+cd $HOME/pkgsrc/bootstrap
 
-./bootstrap --unprivileged --prefix=$HOME/.local --pkgdbdir=$HOME/.local/.pkgdb   \
-            --workdir=$HOME/.work --abi 64 --prefer-pkgsrc=yes --gzip-binary-kit
-
-# remove the resulting configuration file
-# we will use stow instead
-rm $HOME/.local/etc/mk.conf
+./bootstrap --unprivileged \
+            --prefix=$HOME/.local \
+            --workdir=$HOME/.work \
+            --abi 64 \
+            --prefer-pkgsrc=yes \
+            --mk-fragment=$HOME/.work/mk.conf \
+            --gzip-binary-kit=$HOME/.local/bootstrap.tgz
 
 # mount the stowed pkgsrc files
 cd $HOME/.dotfiles
