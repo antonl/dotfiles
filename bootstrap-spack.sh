4b9c579299038977860e35182a49a0cf84b87efe
Anton Loukianov <aloukian@stanford.edu>
2017-12-17 17:00:32 -0800

    add mozilla certs to installed location

--- bootstrap-spack.sh
+++ bootstrap-spack.sh
@@ -13,7 +13,14 @@ mkdir -p $HOME/env
 source $HOME/.bash_profile
 echo "Bootstrapping env/dope (may take a while)"
 spack install python@3.6.3 vim@8.0.1375 +big tmux@2.6
-spack view --verbose hardlink $HOME/env/dope python@3.6.3 vim@8.0.1376 tmux@2.6
+spack view --verbose add $HOME/env/dope python@3.6.3 vim@8.0.1376 tmux@2.6
+
+# use python3.6 as the default python
+ln -s $HOME/env/dope/bin/python3.6 $HOME/env/dope/bin/python
+
+# add mozilla certs to openssl
+SSL_DIR=`spack location --install-dir openssl`
+curl -L -o $SSL_DIR/etc/openssl/cert.pem https://curl.haxx.se/ca/cacert.pem
 
 echo "Bootstrapped spack. Please source your bash profile to see changes."
 echo
